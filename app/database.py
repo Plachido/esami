a02c7d690691477d85b48b2cfe34648a027536ba
Plachido <placidopellegriti@live.it>
2024-11-08 23:15:25 +0100

    Argomenti necessari per evitare chiusura sql

--- app/database.py
+++ app/database.py
@@ -3,4 +3,4 @@ import mysql.connector
 from config import DB_CONFIG
 
 def get_db_connection():
-    return mysql.connector.connect(**DB_CONFIG)
+    return mysql.connector.connect(**DB_CONFIG, auth_plugin='mysql_native_password', use_pure=True)
